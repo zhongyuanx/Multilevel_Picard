aef894de898b17ecdd9c4c349eea023fd67148b8
zhongyuanx <48814340+zhongyuanx@users.noreply.github.com>
2025-11-18 21:14:22 -0600

    Update rbm2.jl

--- rbm2.jl
+++ rbm2.jl
@@ -1,3 +1,5 @@
+#This program implements the O(1) simulation algorithm for the triple (88) in Appendix D of the paper.
+
 using Distributions
 
 function my_rtime(beta, lb=0, ub=Inf)
@@ -9,16 +11,6 @@ function my_rtime(beta, lb=0, ub=Inf)
     else
         error("Beta must be non-negative.")
     end
-    #return lb + (quantile(Normal(), 0.5 * (1 - rand() * (1 - 2 * cdf(Normal(), -sqrt(2 * beta * (ub - lb)))))))^2 / (2 * beta)
-    #return lb + (quantile(Normal(), 0.5 * (1 - my_rand * (1 - 2 * cdf(Normal(), -sqrt(2 * beta * (ub - lb)))))))^2 / (2 * beta)
-end
-
-function my_rtime2(beta, lb=0, ub=Inf)
-    return -1/beta*log(1-rand()*(1-exp(-beta*(ub-lb))))+lb
-end
-
-function my_rtime3(lb, ub, shape)
-    return lb+rand(Truncated(Gamma(shape), 0.0, ub-lb))
 end
 
 function bm_hitting(z, gamma, sigma=1, elapsed=0)
@@ -35,16 +27,7 @@ function bm_hitting(z, gamma, sigma=1, elapsed=0)
         return z^2/sigma^2/std_Gauss^2
     end
 end 
-#     mu = -z/gamma
-#     lambda = z^2/sigma^2
-#     #my_nu = rand(Normal())
-#     my_x = mu + mu^2 * my_nu^2/(2*lambda) - mu/(2*lambda) * sqrt(4*mu*lambda*my_nu^2+mu^2*my_nu^4)
-#     if my_rand <= mu/(mu+my_x)
-#         return my_x
-#     else
-#         return mu^2/my_x
-#     end
-# end
+
 
 
 function stopped_rbm(z, gamma, tau, time, sigma=1, elapsed=0)
@@ -65,6 +48,8 @@ function stopped_rbm(z, gamma, tau, time, sigma=1, elapsed=0)
         #std_Exp = rand(Exponential(1.0))
         my_rbm = sqrt(2 * (sigma^2) * rs_time * (rs_tau - rs_time) / rs_tau * std_Exp + ((rs_tau - rs_time) * z / rs_tau + sigma * sqrt(rs_time * (rs_tau - rs_time) / rs_tau) * std_Gauss)^2)
         my_bm_scaled = ((z^2) * ((rs_time^1.5) / (rs_tau^2) - 2 * sqrt(rs_time) / rs_tau) + 2 * z * sigma * (((rs_tau - rs_time) / rs_tau)^1.5) * std_Gauss + sigma^2 * (rs_tau - rs_time) / rs_tau * sqrt(rs_time) * (std_Gauss^2 + 2 * std_Exp)) / (my_rbm + z) / sigma - gamma * sqrt(rs_time) / sigma
+        # the complicated expressions for my_rbm and my_bm_scaled are due to numerical instabilities associated with their simulations.
+        # this happens when the random time rs_time is very close to zero. 
         my_bm = my_bm_scaled * sqrt(rs_time)
     end
     
